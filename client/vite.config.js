77f44aa6caeb8b478b468cda0d4734f15860838f
Roman Ohiienko <ohiienko.r@gmail.com>
2023-09-26 01:06:05 +0300

    Modify vite.config and remove vite from global deps

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // resolve: {
-  //   alias: [
-  //     { find: "@", replacement: path.resolve(__dirname, "./src") },
-  //     { find: "@root", replacement: path.resolve(__dirname, "./") },
-  //   ],
-  // },
   server: {
     proxy: {
       "/api": {
