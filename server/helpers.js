776601422eb5f9daea0278758f6a59097c969de7
Roman <109099364+ohiienko-r@users.noreply.github.com>
2023-11-18 15:49:44 +0200

    Update helpers.js

--- server/helpers.js
+++ server/helpers.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   service: "Gmail",
   auth: {
-    user: "larnaca.ukrainians@gmail.com",
-    pass: "rqdd smqy wlzm lyvt",
+    user: "userMail",
+    pass: "userpass",
   },
 });
